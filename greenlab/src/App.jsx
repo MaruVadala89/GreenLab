d4d9027ae965d2a93c9044d6c0a543ef034ccc2b
MaruVadala <maruvad15@gmail.com>
2024-05-01 17:05:32 -0300

    ItemListContainer

--- greenlab/src/App.jsx
+++ greenlab/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import IntemListContainer from './components/IntemListContainer/IntemListContainer'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
  
 
   return (
     <>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<IntemListContainer/>}/>
+      <Route path='/:categoryId' element={<IntemListContainer/>}/>
+
+    </Routes>
+    
+    
+    </BrowserRouter>
+
       
     </>
   )
--- greenlab/src/components/IntemListContainer/IntemListContainer.jsx
+++ greenlab/src/components/IntemListContainer/IntemListContainer.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { collection, deleteAllPersistentCacheIndexes, getDocs, getFirestore, query, where } from 'firebase/firestore';
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
+import { useParams } from 'react-router-dom';
 
 
 const IntemListContainer = () => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
+    const { categoryId } = useParams();
 
     useEffect(() => {
 
@@ -28,19 +30,32 @@ const IntemListContainer = () => {
 
             })
             .catch((error) => console.log(error))
-            .finally(() => {setLoading(false)})
+            .finally(() => { setLoading(false) })
 
     }, [categoryId]);
 
 
 
-return (
-    <>
+    return (
+        <>
 
-    { loading ? (<h1>Cargando...</h1>) : (<ItemList products={products}/>)}
+            {loading ? (<h1>Cargando...</h1>) : (
+                <div>
+                    {
+                        products.map((product) => (
+                            <div key={product.id}>
+                                <h2>{product.nombre}</h2>
+                                <img src={product.img}/>
+                                <p>{product.descripción}</p>
+                                <p>Precio: {product.precio}</p>
+                            </div>
+                        ))}
 
-    </>
-);
+                </div>
+            )}
+
+        </>
+    );
 
 }
 export default IntemListContainer;
