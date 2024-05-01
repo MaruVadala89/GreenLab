d9d55c6e2d62b2d80bfb9ccc85d2d13e3bca7906
MaruVadala <maruvad15@gmail.com>
2024-05-01 13:40:15 -0300

    ItemListContainer

---
+++ greenlab/src/components/IntemListContainer/IntemListContainer.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {collection, getDocs, getFirestore, query, where} from 'firebase/firestore';
+
+
+const IntemListContainer = () => {
+    const [products, setProducts] = useState([]) 
+    const [loading, setLoading] = useState(true) 
+
+    useEffect (() => {
+
+        setLoading(true);
+
+        const
+
+
+
+    })
+
+
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default IntemListContainer;
