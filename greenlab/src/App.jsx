9aeff7daef6c7f50efed1f001d7653c98c01467c
MaruVadala <maruvad15@gmail.com>
2024-05-06 17:57:16 -0300

    Cambios

--- greenlab/src/App.jsx
+++ greenlab/src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import IntemListContainer from './components/IntemListContainer/IntemListContainer'
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { CartProvider } from './context/CartContext';
+import NavBoot from './components/NavBoot/NavBoot'
+import Footer from './components/Footer/Footer'
 
 function App() {
  
@@ -10,12 +13,19 @@ function App() {
     <>
 
     <BrowserRouter>
+    <CartProvider>
+      <NavBoot/>
+      
+
     <Routes>
       <Route path='/' element={<IntemListContainer/>}/>
       <Route path='/:categoryId' element={<IntemListContainer/>}/>
       <Route path='/item/:idProduct' element={<ItemDetailContainer/>}/>
-
     </Routes>
+    
+    <Footer/>
+
+    </CartProvider>
     
     
     </BrowserRouter>
